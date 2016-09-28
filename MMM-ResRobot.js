07faae558a8f6e96660dece2e3af290ddb83306e
Johan Alvinger <github@alvinger.com>
2016-09-28 23:34:27 +0200

    Complete rewrite, version 1.0.1

--- MMM-ResRobot.js
+++ MMM-ResRobot.js
@@ -8,13 +8,12 @@
  * based on a script from Michael Teeuw http://michaelteeuw.nl
  * MIT Licensed.
  */
-
 Module.register("MMM-ResRobot",{
 
 	// Define module defaults
 	defaults: {
 		maximumEntries: 6,	// Total Maximum Entries to show
-		updateInterval: 5 * 60 * 1000,	// Update every 5 minutes.
+		updateInterval: 5 * 50 * 1000,	// Update every 5 minutes.
 		animationSpeed: 2000,
 		fade: true,
 		fadePoint: 0.25,	// Start on 1/4th of the list.
@@ -22,12 +21,12 @@ Module.register("MMM-ResRobot",{
 		apiBase: "https://api.resrobot.se/v2/departureBoard?format=json&passlist=0",
 		apiKey: "<YOUR RESROBOT API KEY HERE>",
 		from: "740020749",	// Starting station ID (or array) from ResRobot, default: Stockholm Central Station (Metro)
-		to: "",	// Destination station ID (or array) from ResRobot, default: none
+		to: ""	// Destination station ID (or array) from ResRobot, default: none
 	},
 
 	// Define required styles.
 	getStyles: function() {
-		return ["MMM-ResRobot.css"];
+		return ["MMM-ResRobot.css", "font-awesome.css"];
 	},
 
 	// Define required scripts.
@@ -44,13 +43,24 @@ Module.register("MMM-ResRobot",{
 
 		this.departures = [];
 		this.loaded = false;
+		this.sendSocketNotification("CONFIG", this.config);
 		this.scheduleUpdate(this.config.initialLoadDelay);
-
 		this.updateTimer = null;
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+		console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		if (notification === "DEPARTURES") {
+			console.log("Departures notification received");
+			this.departures = payload;
+			this.loaded = true;
+			this.updateDom();
+		}
 	},
 
 	// Override dom generator.
 	getDom: function() {
+		console.log("getDom: ");	// DEBUG
 		var wrapper = document.createElement("div");
 
 		if (this.config.from === "") {
@@ -60,7 +70,8 @@ Module.register("MMM-ResRobot",{
 		}
 
 		if (!this.loaded) {
-			wrapper.innerHTML = "Loading departures ...";
+			console.log("DEBUG: ");	// DEBUG
+			wrapper.innerHTML = "Fetching departures ...";
 			wrapper.className = "dimmed light small";
 			return wrapper;
 		}
@@ -69,19 +80,25 @@ Module.register("MMM-ResRobot",{
 		table.className = "small";
 
 		for (var d in this.departures) {
+			if (d >= this.config.maximumEntries) {
+				break;
+			}
 			var departure = this.departures[d];
+
+			console.log("Departure no: " + d);
+			console.log(departure.departuretime + " " + departure.line + " " + departure.to);
 
 			var row = document.createElement("tr");
 			table.appendChild(row);
 
 			var depTimeCell = document.createElement("td");
 			depTimeCell.className = "departuretime";
-			depTimeCell.innerHTML = "(" + departure.departuretime + ")";
+			depTimeCell.innerHTML = departure.departuretime;
 			row.appendChild(depTimeCell);
 
 			var depLineCell = document.createElement("td");
 			depLineCell.className = "lineno";
-			depLineCell.innerHTML = departure.from;
+			depLineCell.innerHTML = departure.line;
 			row.appendChild(depLineCell);
 
 			var depToCell = document.createElement("td");
@@ -93,95 +110,17 @@ Module.register("MMM-ResRobot",{
 				if (this.config.fadePoint < 0) {
 					this.config.fadePoint = 0;
 				}
-				var startingPoint = this.departures.length * this.config.fadePoint;
+				var startingPoint = this.config.maximumEntries * this.config.fadePoint;
 				var steps = this.departures.length - startingPoint;
-				if (t >= startingPoint) {
-					var currentStep = t - startingPoint;
+				if (d >= startingPoint) {
+					var currentStep = d - startingPoint;
 					row.style.opacity = 1 - (1 / steps * currentStep);
 				}
 			}
 
 		}
-
 		return table;
 	},
-
-	/* updateTimetable(compliments)
-	 * Requests new data from openweather.org.
-	 * Calls processTrains on succesfull response.
-	 */
-	updateTimetable: function() {
-		var self = this;
-		var retry = true;
-
-		// Reset all departures
-		this.departures = [];
-		var url = this.getURL() + "&id=" + this.config.from;
-
-		var departureRequest = new XMLHttpRequest();
-		departureRequest.open("GET", url, true);
-		departureRequest.onreadystatechange = function() {
-			if (this.readyState === 4) {
-				if (this.status === 200) {
-					self.processDepartures(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.config.id = "";
-					self.updateDom(self.config.animationSpeed);
-
-					Log.error(self.name + ": Incorrect waht so ever...");
-					retry = false;
-				} else {
-					Log.error(self.name + ": Could not load trains.");
-				}
-
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		departureRequest.send();
-	},
-
-	/* getURL()
-	 * Generates a base url with api parameters based on the config.
-	 *
-	 * return String - URL.
-	 */
-	getURL: function() {
-		var url = this.config.apiBase;
-		url +="?key=" + this.config.apiKey;
-		if (this.config.maximumEntries !== "") {
-			url += "&maxJourneys=" + this.config.maximumEntries;
-		}
-
-		return url;
-	},
-
-	/* processDepartures(data)
-	 * Uses the received data to set the various values.
-	 *
-	 * argument data object - Departure information received from ResRobot.
-	 */
-	processTrains: function(data) {
-		var now = moment();
-
-		for (var i in data.Departure) {
-			var departure = data.Departure[i];
-			var departureTime = moment(departure.date + " " + departure.time);
-			var waitingTime = moment.duration(departureTime.diff(now));
-			this.departures.push({
-				timestamp: departureTime,	// Departure timestamp, used for sorting
-				departuretime: departureTime.format("HH:mm"),	// Departure time in HH:mm, used for display
-				waitingtime: waitingTime.get("minutes"),	// Time until departure, in minutes
-				line: departure.transportNumber,	// Line number/name of departure
-				to: departure.direction	// Destination/Direction
-			});
-		}
-
-		this.loaded = true;
-		this.updateDom(this.config.animationSpeed);
-	},
-
 	/* scheduleUpdate()
 	 * Schedule next update.
 	 *
@@ -196,7 +135,7 @@ Module.register("MMM-ResRobot",{
 		var self = this;
 		clearTimeout(this.updateTimer);
 		this.updateTimer = setTimeout(function() {
-			self.updateTimetable();
+			self.updateDom();
 		}, nextLoad);
 	},
 });
---
+++ node_helper.js
@@ -0,0 +1,150 @@
+/* Resrobot - Timetable for ResRobot Module */
+
+/* Magic Mirror
+ * Module: MMM-ResRobot
+ *
+ * By Johan Alvinger https://github.com/Alvinger
+ * based on a script by Benjamin Angst http://www.beny.ch which is
+ * based on a script from Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+const NodeHelper = require("node_helper");
+const unirest = require("unirest");
+const moment = require("moment");
+
+module.exports = NodeHelper.create({
+
+	// Define start sequence.
+	start: function() {
+		console.log("Starting node_helper for module: " + this.name);
+
+		// Set locale.
+		moment.locale(config.language);
+		this.started = false;
+	},
+
+	// Receive notification
+	socketNotificationReceived: function(notification, payload) {
+   		console.log("node_helper for " + this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+		if (notification === "CONFIG" && this.started == false) {
+			this.config = payload;
+			this.started = true;
+			this.scheduleUpdate(this.config.initialLoadDelay);
+		}
+	},
+
+	/* updateTimetable()
+	 * Requests new departure data from ResRobot.
+	 * Calls processDepartures on succesful response.
+	 */
+	updateTimetable: function() {
+		var self = this;
+
+		// Reset all departures
+		this.departures = [];
+
+		// Save list of froms and tos in arrays
+		var destfrom = this.config.from.split(",");
+		var destto   = this.config.to.split(",");
+
+		// Process each from and to pair
+		for (var d in destfrom) {
+			// Get current list of departures between from and to
+			var url = this.getURL() + "&id=" + destfrom[d];
+			if (typeof destto[d] === "string" && destto[d] !== "") {
+				url += "&direction=" + destto[d];
+			}
+			unirest.get(url)
+			.send()
+			.end(function (r) {
+				if (r.error) {
+					console.log(self.name + " : " + r.error);
+				} else {
+					console.log("body: ", JSON.stringify(r.body));
+					self.processDepartures(r.body);
+				}
+			});
+		}
+	},
+
+	/* getURL()
+	 * Generates a base url with api parameters based on the config.
+	 *
+	 * return String - URL.
+	 */
+	getURL: function() {
+		var url = this.config.apiBase;
+		url +="&key=" + this.config.apiKey;
+		if (this.config.maximumEntries !== "") {
+			url += "&maxJourneys=" + this.config.maximumEntries;
+		}
+
+		return url;
+	},
+
+	/* processDepartures(data)
+	 * Uses the received data to set the various values.
+	 *
+	 * argument data object - Departure information received from ResRobot.
+	 */
+	processDepartures: function(data) {
+		console.log("processDepartures: " + this.name);	// DEBUG
+		var now = moment();
+
+		for (var i in data.Departure) {
+			var departure = data.Departure[i];
+			var departureTime = moment(departure.date + " " + departure.time);
+			var waitingTime = moment.duration(departureTime.diff(now));
+			if (waitingTime > 0) {
+				console.log("timestamp: " + departureTime);
+				console.log("departuretime: " + departureTime.format("HH:mm"));
+				console.log("line: " + departure.transportNumber);
+				console.log("to: " + departure.direction);
+				this.departures.push({
+					timestamp: departureTime,	// Departure timestamp, used for sorting
+					departuretime: departureTime.format("HH:mm"),	// Departure time in HH:mm, used for display
+					waitingtime: waitingTime.get("minutes"),	// Time until departure, in minutes
+					line: departure.transportNumber,	// Line number/name of departure
+					to: departure.direction	// Destination/Direction
+				});
+			}
+		}
+		console.log("Number of departures: " + this.departures.length);
+
+		// Sort the departures and schedule an update for the first departure
+		// If no departures, use standard update interval
+		this.departures.sort(function(a, b) {
+			if (a.timestamp < b.timestamp) return -1;
+			if (a.timestamp > b.timestamp) return 1;
+			return 0;
+ 		});
+
+		if (typeof this.departures[0] !== "undefined" && this.departures.length > 0) {
+			var delay = this.departures[0].timestamp - now;
+			this.scheduleUpdate(delay);
+		} else {
+			this.scheduleUpdate();
+		}
+
+		// Tell the main module that we have a new list of departures
+		this.sendSocketNotification("DEPARTURES", this.departures);
+	},
+
+	/* scheduleUpdate()
+	 * Schedule next update.
+	 *
+	 * argument delay number - Milliseconds before next update. If empty, this.config.updateInterval is used.
+	 */
+	scheduleUpdate: function(delay) {
+		var self = this;
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+
+		clearTimeout(this.updateTimer);
+		this.updateTimer = setTimeout(function() {
+			self.updateTimetable();
+		}, nextLoad);
+	}
+});
